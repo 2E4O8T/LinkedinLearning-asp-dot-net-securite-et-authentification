de03c0f2bc2f4b5ad56ce275d3875289ef223a45
2E4O8T <benoit.froidevan@axa.fr>
2024-01-21 18:48:21 +0100

    Add project files.

---
+++ LinkedinLearning/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using LinkedinLearning.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LinkedinLearning.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signinManager) 
+        { 
+            this.userManager = userManager;
+            this.signInManager = signinManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            var result = await this.userManager.CreateAsync(new User
+            {
+                Email = registerVM.Email,
+                UserName = registerVM.Email,
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName
+            }, registerVM.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.Code, item.Description);
+                }
+            }
+            
+            return View(registerVM);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var result = await this.signInManager.PasswordSignInAsync(
+                userName: loginVM.Email,
+                password: loginVM.Password,
+                isPersistent: true,
+                lockoutOnFailure: false
+            );
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("Identifiant incorect", "Identifiant incorrect");
+
+                return View(loginVM);
+            }
+        }
+    }
+}
---
+++ LinkedinLearning/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LinkedinLearning.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkedinLearning.Data
+{
+    public class AppDbContext : IdentityDbContext<User, Role, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
